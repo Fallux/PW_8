5487a81ecf50b0b6f25d6e1d00053f56925bdae3
Roemer Naber <200611@student.glu.nl>
2022-04-19 14:11:18 +0200

    registreren-back-end klaar

--- accountsCode/main.php
+++ accountsCode/main.php
@@ -3,12 +3,15 @@ include './dbConnection.php';
 
 if(!empty($_POST)) {
     $naam = $_POST["naam"];
-    $wachtwoord = $_POST["wachtwoord"];
+    $wachtwoord = md5($_POST["wachtwoord"]);
+    $adres = $_POST["adres"];
+    $postcode = $_POST["postcode"];
+    $telefoonnummer = $_POST["telefoonnummer"];
 
-    $sql = "INSERT INTO `users` (naam, wachtwoord) VALUES ('{$naam}, {$wachtwoord}');";
+    $sql = "INSERT INTO `users` (naam, wachtwoord, adres, postcode, telefoonnummer) VALUES ('{$naam}', '{$wachtwoord}', '{$adres}', '{$postcode}', '{$telefoonnummer}');";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Je bent aangemeld welkom!";
+        // echo "Je bent aangemeld welkom!";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
@@ -28,8 +31,11 @@ if(!empty($_POST)) {
 <body>
     <form action="" method="post">
         <div class="inputContainer">
-            <input type="text" name="naam" class="input" placeholder="naam">
-            <input type="text" name="wachtwoord" class="input" placeholder="wachtwoord">
+            <input type="text" name="naam" class="input" placeholder="Naam">
+            <input type="text" name="wachtwoord" class="input" placeholder="Wachtwoord">
+            <input type="text" name="adres" class="input" placeholder="Adres">
+            <input type="text" name="postcode" class="input" placeholder="Postcode">
+            <input type="text" name="telefoonnummer" class="input" placeholder="Telefoonnummer">
             <input type="submit" value="Send">
         </div>
     </form>
